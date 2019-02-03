c97452b41cc045f309fa1f4ccfa726bcf097fe1b
Vitalik <kovalchuk.vitalik42@gmail.com>
2019-02-03 21:37:08 +0200

    test listen

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+const express = require('express')
 const https = require('https');
 const ViberBot = require('viber-bot').Bot;
 const TextMessage = require('viber-bot').Message.Text;
@@ -8,10 +9,8 @@ const bot = new ViberBot({
     avatar: "https://raw.githubusercontent.com/devrelv/drop/master/151-icon.png"
 });
 
-bot.onTextMessage(/^hi|hello$/i, (message, response) => {
-    response.send(new TextMessage(`Hi there ${response.userProfile.name}. I am ${bot.name}`));
-});
 
 const port = process.env.PORT || 8080;
-const webhookUrl = process.env.WEBHOOK_URL;
-https.createServer(bot.middleware()).listen(port, () => bot.setWebhook(webhookUrl));
+const webhookUrl = "https://viber-bot-gm.herokuapp.com/";
+express().listen(port, () => console.log(`Listening on ${ port }`))
+// https.createServer(bot.middleware()).listen(port, () => bot.setWebhook(webhookUrl));
